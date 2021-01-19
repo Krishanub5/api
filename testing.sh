4d67d5350800d1cd438474628c91c94444f0cecd
Krishanu Bhattacharya <rhel.krishanu@gmail.com>
2021-01-19 11:34:08 +0530

    api test

--- testing.sh
+++ testing.sh
@@ -1,4 +1,4 @@
-status="$(curl -Is http://52.139.201.153:9000/business-2/index.html| head -1)"
+status="$(curl -Is http://52.139.201.153:3010/ | head -1)"
 validate=( $status )
 if [ ${validate[-2]} == "200" ]; then
   echo "OK"
